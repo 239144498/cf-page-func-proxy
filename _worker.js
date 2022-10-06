9b1400ff414d2333a3c7edc09fcb020ead6b1254
Naihe <64341459+239144498@users.noreply.github.com>
2022-10-06 20:17:46 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="4gtvfreepc-cds.cdn.hinet.net";
+        url.hostname="api2.4gtv.tv";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
